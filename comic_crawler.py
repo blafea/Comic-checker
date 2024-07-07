62bed56fdefe8c207e6f0a7db25229f05918bc34
blafea <blafea221651@gmail.com>
2024-07-07 15:41:22 +0800

    Update code and database

--- comic_crawler.py
+++ comic_crawler.py
@@ -1,10 +1,13 @@
 import requests
 from lxml import etree
 
-def check(url):
+
+def check_baozi(url):
     res = requests.get(url)
     html = etree.HTML(res.text)
     # print(res.text)
-    html_find = html.xpath("/html/body/div[@id='__nuxt']/div[@id='__layout']/div[@id='layout']/div[@class='comics-detail']/div[@class='de-info-wr']/div[@class='l-content']/div[@class='pure-g de-info__box']/div[@class='pure-u-1-1 pure-u-sm-2-3 pure-u-md-3-4']/div[@class='comics-detail__info']/div[@class='supporting-text mt-2']")[0]
+    html_find = html.xpath(
+        "/html/body/div[@id='__nuxt']/div[@id='__layout']/div[@id='layout']/div[@class='comics-detail']/div[@class='de-info-wr']/div[@class='l-content']/div[@class='pure-g de-info__box']/div[@class='pure-u-1-1 pure-u-sm-2-3 pure-u-md-3-4']/div[@class='comics-detail__info']/div[@class='supporting-text mt-2']"
+    )[0]
     aaa = etree.tostring(html_find, encoding='utf-8').decode('utf-8')
-    return "小時" in aaa
+    return ("小時" in aaa) or ("今天" in aaa) or ("分鐘" in aaa)
--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from telegram.ext import Updater, CommandHandler
 import datetime
 import comic_crawler
 import json
-import keep_alive
+import requests
+import bs4
+
 
 def hello(update, context):
-    update.message.reply_text('hello, {}'.format(update.message.from_user.first_name))
+    update.message.reply_text('hello, {}'.format(
+        update.message.from_user.first_name))
+
 
 def list(update, context):
     with open("./database.json", 'r') as f:
@@ -14,18 +18,20 @@ def list(update, context):
     for key, value in data.items():
         update.message.reply_text(f'{key}\n{value}')
 
+
 def add(update, context):
     try:
         new = update.message.text[5:].split(" ")
         with open("./database.json", 'r') as f:
             data = json.loads(f.read())
         with open("./database.json", 'w') as f:
-            data.update({new[0]:new[1]})
+            data.update({new[0]: new[1]})
             json.dump(data, f)
             print(data)
         update.message.reply_text(f'{new[0]} 新增成功')
     except:
         update.message.reply_text('新增失敗')
+
 
 def delete(update, context):
     new = update.message.text[5:]
@@ -40,6 +46,7 @@ def delete(update, context):
     except:
         update.message.reply_text('刪除失敗')
 
+
 def check_loop(update):
     send = False
     with open("database.json", "r") as f:
@@ -47,10 +54,12 @@ def check_loop(update):
     for key, value in data.items():
         print(key)
         if comic_crawler.check(value):
-            update.bot.send_message(chat_id="843970308", text="{} 已更新\n{}".format(key, value))
+            update.bot.send_message(chat_id="843970308",
+                                    text="{} 已更新\n{}".format(key, value))
             send = True
     if not send:
         update.bot.send_message(chat_id="843970308", text="沒有漫畫更新")
+
 
 def check_now(update, context):
     send = False
@@ -65,10 +74,30 @@ def check_now(update, context):
     if not send:
         update.message.reply_text("沒有漫畫更新")
 
-updater = Updater('your bot token')
+
+def check_3090(update):
+    URL = "https://www.ptt.cc/bbs/HardwareSale/index.html"
+    response = requests.get(URL)
+
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    header = soup.find_all("div", class_="title")
+
+    for h in header:
+        if ("3090" in h.text) and ("賣" in h.text.replace("賣貨便", "")):
+            update.bot.send_message(chat_id="843970308", text="find 3090")
+        else:
+            print("no")
+
+
+updater = Updater('6269578873:AAE6exmJI8M74lgEJD1S-LOOQhWSXE8OEsc', )
 j = updater.job_queue
 # j.run_repeating(check, interval=10)
-j.run_repeating(check_loop, interval=datetime.timedelta(days=1), first=datetime.time(hour=15, minute=59))
+j.run_repeating(check_loop,
+                interval=datetime.timedelta(hours=12),
+                first=datetime.time(hour=15, minute=59))
+# j.run_repeating(check_3090,
+#                 interval=datetime.timedelta(minutes=5),
+#                 first=datetime.time())
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(CommandHandler('add', add))
 updater.dispatcher.add_handler(CommandHandler('del', delete))
@@ -76,37 +105,6 @@ updater.dispatcher.add_handler(CommandHandler('list', list))
 updater.dispatcher.add_handler(CommandHandler('check', check_now))
 
 if __name__ == "__main__":
-    keep_alive.keep_alive()
     updater.start_polling()
     updater.idle()
     updater.stop()
-
-# def hello(bot, update):
-#     update.message.reply_text('hello, {}'.format(update.message.from_user.first_name))
-
-# def add(bot, update):
-#     new = update.message.text[5:].split(" ")
-#     with open("./database.json", 'r+') as f:
-#         data = json.loads(f.read())
-#         data.update({new[0]:new[1]})
-#         json.dump(data)
-#         print(data)
-
-# def check(bot, update):
-#     while True:
-#         with open("database.json", "r") as f:
-#             data = json.loads(f.read())
-#         for key, value in data:
-#             if comic_crawler.check(value):
-#                 update.message.reply_text("{}:{}".format(key, value))
-#         time.sleep(10)
-    
-
-# updater = Updater('6269578873:AAE6exmJI8M74lgEJD1S-LOOQhWSXE8OEsc')
-
-# updater.dispatcher.add_handler(CommandHandler('hello', hello))
-# updater.dispatcher.add_handler(CommandHandler('add', add))
-# updater.dispatcher.add_handler(CommandHandler('check', check))
-
-# updater.start_polling()
-# updater.idle()
